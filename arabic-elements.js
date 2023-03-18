bec208f67faff4c24fd0bf9688e6ca9d7989bf9b
WADev00 <128231402+WADev00@users.noreply.github.com>
2023-03-18 16:02:45 +0300

    Update arabic-elements.js

--- arabic-elements.js
+++ arabic-elements.js
@@ -1,10 +1,1 @@
-var arabicElements = {
-  'نص': 'h1',
-  'سطر': 'hr'
-};
-
-Object.keys(arabicElements).forEach(function(key) {
-  var arabicElement = document.createElement(key);
-  var englishElement = arabicElements[key];
-  document.createElement(englishElement);
-});
+alert("Hi")
